2cc5168152a6d8cd10a87a5c15c0af25db0ffa1e
Mileto Di Marco <mileto.di.marco@howest.be>
2024-03-21 13:08:15 +0100

    Add AuthController to api project

---
+++ Pri.Api.Music.Api/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Api.Music.Core.Entities;
+
+namespace Pri.Api.Music.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login()
+        {
+            //authenticate the user
+            //get the claims
+            //generate the token
+            //return the token
+            return Ok();
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+    }
+}
